38e048607ece1093f8748d45492a7b005d143c86
mukbe <azure9312@gmail.com>
2017-10-13 21:26:17 +0900

    unit임시

--- unit.h
+++ unit.h
@@ -1,9 +1,16 @@
 #pragma once
 #include "gameObject.h"
+#include <vector>
+//static vector2D up(0, -1);
+//static vector2D down(0, 1);
+//static vector2D right(1,0);
+//static vector2D left(-1,0);
 
 
 class unit : public gameObject
 {
+private:
+	vector2D _index;
 public:
 	unit();
 	~unit();
@@ -12,5 +19,7 @@ public:
 	void release();
 	void update();
 	void render();
+
+	void moveCallBack(POINT dest);
 };
 
