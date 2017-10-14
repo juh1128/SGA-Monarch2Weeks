a61418edbd902e59f9ffc13e33f9fafe3cc9b386
mukbe <azure9312@gmail.com>
2017-10-14 17:02:57 +0900

    unit 상태객체패턴 추가  dset로 스무스하게 이동하는것까지만

--- unit.cpp
+++ unit.cpp
@@ -17,12 +17,13 @@ HRESULT unit::init()
 
 	_pos.x = _pos.y = _index.x = _index.y = 0;
 
-	this->addCallback("move", [this](tagMessage msg)
+	this->addCallback("move", [&](tagMessage msg)
 	{
-		this->moveCallBack(msg.ptData);
+		this->moveCallBack(msg.targetList[0]);
 	});
 	
-
+	_unitState = new unitNoneState;
+	_moveSpeed = 3.0f;
 	return S_OK;
 }
 
@@ -36,9 +37,13 @@ void unit::update()
 
 	if (KEYMANAGER->isOnceKeyDown(VK_LBUTTON))
 	{
-		POINT temp = WORLD->getMap()->getPickedTile()->getIndex();
-		this->sendMessage("move", 0, 0, 0, (POINT)temp);
+		gameObject* temp = WORLD->getMap()->getPickedTile();
+		vector<gameObject*> vr;
+		vr.push_back(temp);
+		this->sendMessage("move", 0, 0, 0, POINT(),vr);
+		vr.clear();
 	}
+	_unitState->update(*this);
 }
 
 void unit::render()
@@ -48,8 +53,43 @@ void unit::render()
 
 }
 
-void unit::moveCallBack(POINT dest)
+void unit::moveCallBack(gameObject* dest)
 {
-	_index =  (vector2D)dest;
-	_pos = tileMap::getTilePosFromIndex(_index);
+	changeState(new unitOneStep((terrainTile*)dest));
+
+}
+
+void unit::changeState(unitState* newstate)
+{
+	SAFE_DELETE(_unitState);
+	_unitState = newstate;
+	_unitState->enter(*this);
+}
+
+void unitNoneState::update(unit & unit)
+{
+}
+
+void unitOneStep::enter(unit & unit)
+{
+	//목적지를 못받아옴 에러 or 이상한곳찍음
+	if (!_destTile) unit.changeState(new unitNoneState);
+
+	_destPos = WORLD->getMap()->getTilePosFromIndex(_destTile->getIndex());
+}
+
+void unitOneStep::update(unit & unit)
+{
+	vector2D dis = _destPos - unit._pos;
+	_moveRatio = WORLD->getTileMoveRatio(unit._index.x, unit._index.y);
+	if (dis.getLength() <= unit._moveSpeed * _moveRatio)
+	{
+		unit._pos = _destPos;
+		unit.changeState(new unitNoneState);
+	}
+	else
+	{
+		unit._pos = unit._pos + dis.normalize()*unit._moveSpeed*_moveRatio;
+
+	}
 }
--- unit.h
+++ unit.h
@@ -1,16 +1,26 @@
 #pragma once
 #include "gameObject.h"
 #include <vector>
-//static vector2D up(0, -1);
-//static vector2D down(0, 1);
-//static vector2D right(1,0);
-//static vector2D left(-1,0);
+//static vector2D up(2, -1);
+//static vector2D down(-2, 1);
+//static vector2D right(2,1);
+//static vector2D left(-2,-1);
 
+class unitState;
 
 class unit : public gameObject
 {
 private:
 	vector2D _index;
+
+	unitState* _unitState;
+
+	float _moveSpeed;
+
+	friend class unitNoneState;
+	friend class unitOneStep;
+
+
 public:
 	unit();
 	~unit();
@@ -20,6 +30,43 @@ public:
 	void update();
 	void render();
 
-	void moveCallBack(POINT dest);
+	void moveCallBack(gameObject* dset);
+	void changeState(unitState* newstate);
 };
 
+class unitState
+{
+private:
+public:
+	virtual ~unitState() {}
+
+	virtual void enter(unit& unit) {}
+	virtual void update(unit& unit) {}
+};
+
+class unitNoneState : public unitState
+{
+private:
+public:
+
+	virtual void enter(unit& unit) {}
+	virtual void update(unit& unit);
+
+};
+
+class unitOneStep : public unitState
+{
+private:
+	vector2D _destPos;
+	terrainTile* _destTile;
+	int _destheight;
+	float _moveRatio;
+public:
+	unitOneStep(terrainTile* tile)
+	{
+		_destTile = tile;
+	}
+	virtual void enter(unit& unit);
+	virtual void update(unit& unit);
+
+};
