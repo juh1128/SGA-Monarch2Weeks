05f8a18c627c9d27dd00e76851cff75d88310e82
mukbe <azure9312@gmail.com>
2017-10-21 19:29:45 +0900

    머지 수정 전투수정

--- unitMerge.cpp
+++ unitMerge.cpp
@@ -71,83 +71,87 @@ void unitMerge::enter(unit& me)
 		}
 
 
-		//====================================
-		// - 병합할 대상에게 추적하는 상황
+		////====================================
+		//// - 병합할 대상에게 추적하는 상황
 
-		//방향정하기
-		int xDistance = UTIL::getDistance(me._index.x, 0, me._mergeUnit->_index.x, 0);
-		int yDistance = UTIL::getDistance(0, me._index.y, 0, me._mergeUnit->_index.y);
-		vector2D destIndex = me._index;
-		bool isCanGo = true;
+		//머지 상대의 인덱스가 바뀌는지 체크할것 코딩해야됨
+		me.moveAstar(me._mergeUnit->_index.x,me._mergeUnit->_index.y);
 
-		//x가 더길어서 x로 가야하는 상황
-		if(xDistance >= yDistance)
-		{
-			//내가 왼쪽에있다면
-			if (me._index.x < me._mergeUnit->_index.x)
-			{
-				destIndex.x += 1;
-			}
-			else if (me._index.x > me._mergeUnit->_index.x)
-			{
-				destIndex.x -= 1;
-			}
+		////방향정하기
+		//int xDistance = UTIL::getDistance(me._index.x, 0, me._mergeUnit->_index.x, 0);
+		//int yDistance = UTIL::getDistance(0, me._index.y, 0, me._mergeUnit->_index.y);
+		//vector2D destIndex = me._index;
+		//bool isCanGo = true;
 
-			if (me.isMoveable(destIndex.toPoint()))
-			{
-				me.changeState(new unitOneStep(destIndex.x, destIndex.y));
-				return;
-			}
-			else
-			{
-				vector2D index;
-				for (int i = 0; i < 4; i++)
-				{
-					index = me._index+UnitDirection::DIRECTION(i);
-					if (me.isMoveable(index.toPoint()))
-					{
-						break;
-					}
-				}
-				me.changeState(new unitOneStep(index.x, index.y));
-				return;
-			}
+		////x가 더길어서 x로 가야하는 상황
+		//if(xDistance >= yDistance)
+		//{
+		//	//내가 왼쪽에있다면
+		//	if (me._index.x < me._mergeUnit->_index.x)
+		//	{
+		//		destIndex.x += 1;
+		//	}
+		//	else if (me._index.x > me._mergeUnit->_index.x)
+		//	{
+		//		destIndex.x -= 1;
+		//	}
 
-		}
+		//	if (me.isMoveable(destIndex.toPoint()))
+		//	{
+		//		me.changeState(new unitOneStep(destIndex.x, destIndex.y));
+		//		return;
+		//	}
+		//	else
+		//	{
+		//		vector2D index;
+		//		for (int i = 0; i < 4; i++)
+		//		{
+		//			index = me._index+UnitDirection::DIRECTION(i);
+		//			if (me.isMoveable(index.toPoint()))
+		//			{
+		//				break;
+		//			}
+		//		}
+		//		me.changeState(new unitOneStep(index.x, index.y));
+		//		return;
+		//	}
 
-		if (xDistance < yDistance)
-		{
-			//내가 왼쪽에있다면
-			if (me._index.y < me._mergeUnit->_index.y)
-			{
-				destIndex.y += 1;
-			}
-			else if (me._index.y > me._mergeUnit->_index.y)
-			{
-				destIndex.y -= 1;
-			}
+		//}
 
-			if (me.isMoveable(destIndex.toPoint()))
-			{
-				me.changeState(new unitOneStep(destIndex.x, destIndex.y));
-				return;
-			}
-			else
-			{
-				vector2D index;
-				for (int i = 0; i < 4; i++)
-				{
-					index = me._index + UnitDirection::DIRECTION(i);
-					if (me.isMoveable(index.toPoint()))
-					{
-						break;
-					}
-				}
-				me.changeState(new unitOneStep(index.x, index.y));
-				return;
-			}
+		//if (xDistance < yDistance)
+		//{
+		//	//내가 왼쪽에있다면
+		//	if (me._index.y < me._mergeUnit->_index.y)
+		//	{
+		//		destIndex.y += 1;
+		//	}
+		//	else if (me._index.y > me._mergeUnit->_index.y)
+		//	{
+		//		destIndex.y -= 1;
+		//	}
 
-		}
+		//	if (me.isMoveable(destIndex.toPoint()))
+		//	{
+		//		me.changeState(new unitOneStep(destIndex.x, destIndex.y));
+		//		return;
+		//	}
+		//	else
+		//	{
+		//		vector2D index;
+		//		for (int i = 0; i < 4; i++)
+		//		{
+		//			index = me._index + UnitDirection::DIRECTION(i);
+		//			if (me.isMoveable(index.toPoint()))
+		//			{
+		//				break;
+		//			}
+		//		}
+		//		me.changeState(new unitOneStep(index.x, index.y));
+		//		return;
+		//	}
+
+		//}
+
 	}
 }
 
